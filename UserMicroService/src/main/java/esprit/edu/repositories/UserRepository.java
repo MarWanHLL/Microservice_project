3ebbeec9d27cf503238a215aeafec55ed0f77e9f
MarWanHLL <helalimarwan1919@gmail.com>
2025-11-12 11:14:11 +0100

    first commit

--- UserMicroService/src/main/java/esprit/edu/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package esprit.edu.repositories;
-
-import esprit.edu.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
